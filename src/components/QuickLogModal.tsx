3f9fca712f5bb9d1f70f9d522b399e615b819795
Emrikur <151168924+Emrikur@users.noreply.github.com>
2026-09-07 22:21:03 +0200

    fix: restrict date picker inputs to present and past dates only in QuickLogModal and TimeReport components

--- src/components/QuickLogModal.tsx
+++ src/components/QuickLogModal.tsx
@@ -185,7 +185,10 @@ return
           {dbResponse ? dbResponse.filter((company) => company.is_active).map((company) => <option className="form-option" value={company.id} key={company.id}>{company.name}</option>): null}
         </select>
 
-          <input onClick={(e) => e.currentTarget.showPicker()} className="input-date" name="date"  type="date" />
+          <input
+          max={new Date().toISOString().split('T')[0]}
+          onClick={(e) => e.currentTarget.showPicker()}
+          className="input-date" name="date"  type="date" />
 
           <p className="peptalk">*{pepTalk}*</p>
           <div className="hours">
